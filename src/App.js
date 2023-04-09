2b24885657c91871ed1bda792508bce51d73f876
Damiandesign93 <112899588+Damiandesign93@users.noreply.github.com>
2023-04-09 20:42:38 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import NoFound from './components/NoFound';
+import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
+import Cart from './components/Cart';
+import CartContextProvider from "./context/CartContext";
+
+function App() {
+  return (
+    <CartContextProvider>
+      <BrowserRouter>
+          <Navbar/>
+          <Routes>
+              <Route path="/" element={<ItemListContainer/>} />  
+              <Route path="/category/:categoryId" element={<ItemListContainer/>} />
+              <Route path="/item/:itemId" element={<ItemDetailContainer/>} />
+              <Route path="*" element={<NoFound/>} />
+              <Route path="/cart" element={<Cart/>} />
+          </Routes>
+      </BrowserRouter>
+    </CartContextProvider>
+    );
+}
+
+export default App;
